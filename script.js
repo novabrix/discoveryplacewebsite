ab073686080f2e1b760a2733d9c82d6d0cef1591
ayuubthegreat <yusufayuub00@gmail.com>
2025-04-28 16:28:39 -0500

    push git main

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 "use strict";
+const contactUs = document.querySelector(`.contactus`)
+const contactButton = document.querySelector(`.contact`)
+const overlay = document.querySelector(`.overlay`)
+
+contactButton.addEventListener(`click`, function() {
+    contactUs.classList.remove(`hidden`)
+    overlay.classList.remove(`hidden`)
+    console.log(`Button pressed`);
+})
+
+overlay.addEventListener(`click`, function() {
+    contactUs.classList.add(`hidden`)
+    overlay.classList.add(`hidden`)
+    console.log(`Document pressed`);
+})
