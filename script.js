2296d9cd7ca539adbd4a0d2b89d880cfbcbc769b
ayuubthegreat <yusufayuub00@gmail.com>
2025-05-13 23:00:12 -0500

    push git main

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 const contactUs = document.querySelector(`.contactus`)
 const contactButton = document.querySelector(`.contact`)
 const overlay = document.querySelector(`.overlay`)
-
+const phoneNumberBottom = document.querySelectorAll(`.phonenumber`)
+const emailAddress = document.querySelectorAll(`.email`)
+const credits = document.querySelector(`.credits-bar`)
 contactButton.addEventListener(`click`, function() {
     contactUs.classList.remove(`hidden`)
     overlay.classList.remove(`hidden`)
@@ -14,3 +16,15 @@ overlay.addEventListener(`click`, function() {
     overlay.classList.add(`hidden`)
     console.log(`Document pressed`);
 })
+const phoneNumber = `612-242-3440`
+const email = `yusufayuub00@gmail.com`
+
+console.log(phoneNumberBottom);
+for (const i of phoneNumberBottom) {
+    i.textContent = `Phone Number: ${phoneNumber}`
+}
+for (const i of emailAddress) {
+    i.textContent = `Email Address: ${email}`
+}
+
+credits.textContent = `Made by Ayuub and Idriis Yusuf.`
